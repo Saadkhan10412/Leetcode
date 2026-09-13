8699b90363255ec4495d0e79a5a84f66f3241d02
Saad KHAN <saadkhan10412@gmail.com>
2026-09-13 17:24:11 +0530

    Time: 1922 ms (5.02%), Space: 261.6 MB (18.01%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,28 +1,29 @@
 class Solution{
 public:
-    bool helper(vector<int>& stones,vector<vector<int>>& dp,int i,int k){
-        if(i==stones.size()-1)
-            return true;
-        if(dp[i][k]!=-1)
-            return dp[i][k];
-
-        bool a=false,b=false,c=false;
-
-        for(int j=i+1;j<stones.size();j++){
-            if(stones[j]==stones[i]+k-1)
-                a=helper(stones,dp,j,k-1);
-            if(stones[j]==stones[i]+k)
-                b=helper(stones,dp,j,k);
-            if(stones[j]==stones[i]+k+1)
-                c=helper(stones,dp,j,k+1);
-        }
-
-        return dp[i][k]=a||b||c;
-    }
-
     bool canCross(vector<int>& stones){
         int n=stones.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return helper(stones,dp,0,0);
+        vector<vector<int>> dp(n,vector<int>(n+1,false));
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<n;i++)
+            mp[stones[i]]=i;
+
+        for(int k=0;k<=n;k++)
+            dp[n-1][k]=true;
+
+        for(int i=n-2;i>=0;i--){
+            for(int k=0;k<=n;k++){
+                if(k-1>0&&mp.find(stones[i]+k-1)!=mp.end())
+                    dp[i][k]=dp[i][k]||dp[mp[stones[i]+k-1]][k-1];
+
+                if(k>0&&mp.find(stones[i]+k)!=mp.end())
+                    dp[i][k]=dp[i][k]||dp[mp[stones[i]+k]][k];
+
+                if(k+1<=n&&mp.find(stones[i]+k+1)!=mp.end())
+                    dp[i][k]=dp[i][k]||dp[mp[stones[i]+k+1]][k+1];
+            }
+        }
+
+        return dp[0][0];
     }
 };
